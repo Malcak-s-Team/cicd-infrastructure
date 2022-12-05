b3b8c2349a42dd50abbe8c683bc4ea974f7e323b
Malcak <malcakx@gmail.com>
2022-12-05 02:37:59 -0500

    move jenkins files to subfolder

--- scripts/download-s3.sh
+++ scripts/download-s3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 aws s3 cp s3://malcak-jenkins-state/backup.tar.gz /home/ec2-user/
 sudo docker run --rm -v /home/ec2-user/:/backup busybox tar -xzvf /backup/backup.tar.gz -C /backup/
-sudo mv /home/ec2-user/var/jenkins_home /home/ec2-user/jenkins-volume
+sudo mv /home/ec2-user/var/jenkins_home /home/ec2-user/jenkins/jenkins-volume
 sudo rm -rf /home/ec2-user/var /home/ec2-user/backup.tar.gz
--- scripts/launch-jenkins.sh
+++ scripts/launch-jenkins.sh
@@ -1,5 +1,5 @@
 # !/bin/bash
-cat <<EOF > /home/ec2-user/docker-compose.yml
+cat <<EOF > /home/ec2-user/jenkins/docker-compose.yml
 services:
   jenkins:
     container_name: jenkins
@@ -12,6 +12,6 @@ services:
       - 50000:50000
     volumes:
       - "/var/run/docker.sock:/var/run/docker.sock"
-      - "/home/ec2-user/jenkins-volume:/var/jenkins_home"
+      - "/home/ec2-user/jenkins/jenkins-volume:/var/jenkins_home"
 EOF
 sudo docker compose -f /home/ec2-user/docker-compose.yml up -d
