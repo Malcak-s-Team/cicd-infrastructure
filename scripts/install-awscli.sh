932c883a8a9a82ff96d41ff2b0011fd9d74fbfc5
Malcak <malcakx@gmail.com>
2022-12-06 18:21:42 -0500

    improve awscli install

--- scripts/install-awscli.sh
+++ scripts/install-awscli.sh
@@ -4,12 +4,3 @@ sudo unzip awscliv2.zip
 sudo ./aws/install
 sudo rm -rf awscliv2.zip
 sudo rm -rf ./aws
-mkdir -p /home/ec2-user/.aws
-touch /home/ec2-user/.aws/credentials
-cat <<EOF > /home/ec2-user/.aws/credentials
-[default]
-aws_access_key_id = ${AWS_ACCESS_KEY_ID}
-aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
-EOF
-sudo chmod 0600 /home/ec2-user/.aws/credentials
-sudo chown root:root /home/ec2-user/.aws/credentials
