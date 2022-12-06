641e0c8eb1f3577a26516320a187c33794f44fc3
Malcak <malcakx@gmail.com>
2022-12-06 18:21:55 -0500

    remove wget from terraform install

--- scripts/install-terraform.sh
+++ scripts/install-terraform.sh
@@ -1,4 +1,5 @@
 # !/bin/bash
 sudo yum install -y yum-utils
-sudo wget https://releases.hashicorp.com/terraform/1.3.5/terraform_1.3.5_linux_amd64.zip
+sudo curl "https://releases.hashicorp.com/terraform/1.3.5/terraform_1.3.5_linux_amd64.zip" -o "terraform_1.3.5_linux_amd64.zip"
 sudo unzip ./terraform_1.3.5_linux_amd64.zip -d /usr/local/bin
+sudo rm -rf ./terraform_1.3.5_linux_amd64.zip
